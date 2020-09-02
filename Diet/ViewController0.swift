f22ca9274378c0f6b93c56bca32349b66d0a0075
Akito0902 <zerosensen@icloud.com>
2020-09-02 14:54:48 +0900

    旧ファイル追加

---
+++ Diet/ViewController0.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController0.swift
+//  Diet
+//
+//  Created by 窪田　彬人 on 2020/08/31.
+//  Copyright © 2020 大人の部活P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController0: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
